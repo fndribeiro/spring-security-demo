66abbf843043f7fdd46f7dbf9aa3e375b92b462a
Fernando Ribeiro <fribeiro@ibm.com>
2022-04-19 22:07:15 -0300

    Initial commit.

---
+++ src/main/java/br/com/ribeiro/fernando/springsecuritydemo/ports/controllers/TestController.java
@@ -0,0 +1,16 @@
+package br.com.ribeiro.fernando.springsecuritydemo.ports.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+	
+	@GetMapping
+	public String test() {
+		return "Application is up and running!";
+	}
+	
+}
---
+++ src/test/java/br/com/ribeiro/fernando/springsecuritydemo/SpringSecurityDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ribeiro.fernando.springsecuritydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
